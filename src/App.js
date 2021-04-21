2fdce87673a0d507372546022d2b8545851e1f13
arhamumar94 <arhamumar1999@gmail.com>
2021-04-21 17:40:52 +0530

    hey

--- src/App.js
+++ src/App.js
@@ -70,7 +70,7 @@ function App() {
           }}>
           
           </Route>
-          <Route  path="/about">
+          <Route exact path="/about">
             <About />
           </Route>
      </Switch>
